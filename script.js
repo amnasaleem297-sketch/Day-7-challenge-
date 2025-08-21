80d2d8f13aa05fcb533fb367d3871c38241ca745
amnasaleem297-sketch <amnasaleem297@gmail.com>
2025-08-21 05:17:26 +0300

    got updated

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let name = "amna";
+let age = 18;
+const city = "Kuwait";
+
+console.log("%c" +name, "color:green;");
+console.log("%c" +age, "color:green;");
+console.log("%c" +city, "color:green;");
+
+let yourName = prompt("Whats your name?");
+let yourAge = prompt("your age?");
+
+alert("Hello " + yourName+" your age is " +yourAge )
